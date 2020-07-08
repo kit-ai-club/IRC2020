f22c034375058920a82edf96a852f81037782568
SasakiHidetaka <hide.zazenbo@gmail.com>
2020-07-09 02:39:56 +0900

    update prototype.py

--- prototype.py
+++ prototype.py
@@ -10,50 +10,26 @@ import matplotlib.pyplot as plt
 import h5py
 from keras.models import Sequential, Model  # kerasというライブラリのmodelsパッケージにある、Sequential, Modelという何か（ファイル or 関数 or クラス）を使うよ～
 import keras.layers as layers
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import glob
+import gc
+
 # pycharmでは、F4 を押すと、引用元・定義元に飛べるので使ってみよう
 
 
 """
-データの取得
+data path
 """
-# data-fileの場所（google-colabの場合、google-driveのルートが、"drive/My Drive" となる）
-data_path = "data"
-train_h5_path = os.path.join(data_path, "food_c101_n10099_r64x64x3.h5")  # train-data   pathの文字列を確認してみよう
-test_h5_path = os.path.join(data_path, "food_test_c101_n1000_r64x64x3.h5")  # test-data
-# windowsとlinuxで、スラッシュとバックスラッシュの違いがあることを気にしないために、 os.path.join を使う
+data_path = os.path.join("data")  # colabの場合、google-driveのルートが、"drive/My Drive" となる
+train_path = os.path.join(data_path, "train")
+test_path = os.path.join(data_path, "test")
 
-
-# train-data
-with h5py.File(train_h5_path, 'r') as file:  # train-dataを 'r' = read mode で読み込んで、変数fileに格納
-    print(file.keys())  # .h5 形式では、関数keys()で中身が見れる
+# example
+ex_path = os.path.join(train_path, "train_256_0.h5")
+with h5py.File(ex_path, 'r') as file:  # train-dataを 'r' = read mode で読み込んで、変数fileに格納
     x_train = file['images'].value  # imagesにアクセス。さらに、.value と書くとnumpy形式にしてくれる
     y_train = file['category'].value  # 教師データ（正解データ）。すでにone-hot vector になっている
-    category_names = file['category_names'].value  # str型ではなく、bytes型で格納されている。見てみよう
-    label_names = [x.decode() for x in file['category_names'].value]  # 全カテゴリ名を格納。bytes型は、decode()でstrに変換できる。
-    print(label_names)
-# with構文で開いたファイルは、構文が終われば自動的に閉じられる。
-
-# x_train = (10099, 64, 64, 3), y_train = (10099, 101)
-#  ↑ このようにshapeを調べてメモしておくと便利
-
-
-# test-data
-with h5py.File(test_h5_path, 'r') as file:
-    print(file.keys())
-    x_test = file['images'].value
-    y_test = file['category'].value
-
-
-def gray_scale(x):
-    # Y = 0.299 * R + 0.587 * G + 0.114 * B
-    y = 0.299 * x[:, :, :, 0] + 0.587 * x[:, :, :, 1] + 0.114 * x[:, :, :, 2]  # x_train = (10099, 64, 64, 3)
-    return 255 - y  # yは輝度なので255-y
-
-
-# グレースケールにする（全結合層は、3チャンネル(RGB)の情報を扱えないため）
-x_train_gray = gray_scale(x_train)  # shapeを確認しよう
-x_test_gray = gray_scale(x_test)
-
 
 """
 【matplotlib.pyplotの基本】
@@ -64,60 +40,106 @@ x_test_gray = gray_scale(x_test)
 ④show
 """
 # 画像チェック
-fig = plt.figure(figsize=(10, 5))  # figure-sizeはインチ単位
-ax = fig.add_subplot(121)  # Figure内にAxesを追加。121 =「1行2列のaxesを作って、その1番目(1列目)をreturnしろ」
+fig = plt.figure(figsize=(3, 3))  # figure-sizeはインチ単位
+ax = fig.add_subplot(111)  # Figure内にAxesを追加。121 =「1行2列のaxesを作って、その1番目(1列目)をreturnしろ」
 ax.imshow(x_train[0])  # 画像ならimshow()
-ax = fig.add_subplot(122)
-ax.imshow(x_train_gray[0], cmap='Greys')  # gray-scaleデータなので追加の引数
 plt.show()  # 最後はpltに戻る
 
-
 """
-データの加工
-"""
-# 全結合層を使うので、画像を1次元化。画像データの行列を短冊状に切ってベクトル化。
-x_train_gray = x_train_gray.reshape(10099, 64 * 64)  # もともと (10099, 64, 64) だったのを、(10099, 64 * 64) に。
-x_test_gray = x_test_gray.reshape(1000, 64 * 64)
-
-# 画素を0~1の範囲に変換(正規化)。入力される値は０付近でないと、訓練が安定しない。
-x_train_gray = x_train_gray / 255
-x_test_gray = x_test_gray / 255
-
-"""
-keras Sequential での流れ
-①model = Sequential()  箱の作成
-②model.add()         　層の追加
-③model.compile()     　設定
-④model.fit()  　　　　　訓練
-⑤model.evaluate()　　 　評価
+keras Functinal API での流れ
+①Input()            入力の作成
+②x = f(x)         　層の追加・出力の作成
+③model = Model()    箱の作成
+④model.compile()     設定
+⑤model.fit()  　　　  訓練
+⑥model.evaluate()　　 評価
 """
 
-# Sequentialは、モデルを入れる箱のクラス。
-model = Sequential()
-model.add(layers.Dense(512, activation='relu', input_dim=64 * 64))  # Dense＝全結合層。activation＝活性化関数。input_dim＝入力次元＝入力ノードの数。512＝出力ノードの数。
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(101, activation='softmax'))  # 最終層のactivationは、確率を出すためにsoftmax。101クラスの分類。
+inputs = layers.Input(shape=(256, 256, 3))
+x = layers.Conv2D(64, kernel_size=3, padding='same', activation='relu')(inputs)
+x = layers.MaxPool2D(2)(x)
+x = layers.Conv2D(128, kernel_size=3, padding='same', activation='relu')(x)
+x = layers.MaxPool2D(2)(x)
+x = layers.Conv2D(256, kernel_size=3, padding='same', activation='relu')(x)
+x = layers.MaxPool2D(2)(x)
+x = layers.Conv2D(512, kernel_size=3, padding='same', activation='relu')(x)
+x = layers.MaxPool2D(2)(x)
+x = layers.Conv2D(1024, kernel_size=3, padding='same', activation='relu')(x)
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dense(101, activation='softmax')(x)
 
-# ちなみに、「次元」というのは、1層あたりのノードの数＝ベクトルの要素数や、画像データのピクセル数などを表す。
-# （「画像データ＝2次元」という表現とはまた違う「次元」の話）
-
+model = Model(inputs=inputs, outputs=x)
 
 # 各種設定
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])  # metrics=評価関数、acc=accuracy
 
+# メモリリーク対策のグローバル変数
+x, y, datagen, generator = None, None, None, None  # いじらない
 
-# 訓練の実行
-epochs = 10
+
+def flow_from_h5(directory, batch_size, data_aug=False):
+    """
+    directory内のh5ファイルを順に読み込んで出力するジェネレータ。
+    ImageDataGeneratorの部分以外はいじらない。
+    """
+
+    files = glob.glob(os.path.join(directory, '*.h5'))
+    while True:
+        for file in files:
+            global x, y, datagen, generator
+            del x, y, datagen, generator
+            gc.collect()
+
+            with h5py.File(file, 'r') as f:
+                x = f['images'].value
+                y = f['category'].value
+
+            if not data_aug:
+                datagen = ImageDataGenerator(rescale=1 / 255.)  # いじらない。rescaleで画像を正規化している。
+            else:
+                datagen = ImageDataGenerator(rescale=1 / 255.)  # DataAugmentationするなら、引数（rescale以外）をいじる。
+
+            generator = datagen.flow(
+                x,
+                y,
+                batch_size=batch_size,
+                shuffle=True, )
+            epoch_per_file = x.shape[0] // batch_size + 1
+            for e in range(epoch_per_file):
+                yield next(generator)
+
+
+"""
+画像を256*256にしてから激重なので、pycharm上でデバッグしたいときは、
+以下4つの値を全て小さめに設定すれば、動きが確認できる程度に軽くなるはず（colabで訓練するときは値を戻すことに注意）
+"""
+epochs = 100
 batch_size = 100
-history = model.fit(x=x_train_gray, y=y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-# historyに訓練の推移のデータが格納される
+steps_per_epoch = 75750 // batch_size + 1  # 元は 75750 // batch_size + 1
+validation_steps = 25250 // batch_size + 1  # 元は  25250 // batch_size + 1
 
+train_generator = flow_from_h5(train_path, batch_size, data_aug=True)
+test_generator = flow_from_h5(test_path, batch_size, data_aug=False)
+
+"""
+以降は、原則いじらない
+"""
+# 訓練
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=steps_per_epoch,
+    epochs=epochs,
+    validation_data=test_generator,
+    validation_steps=validation_steps,
+    workers=0)
 
 # 評価
-score = model.evaluate(x_test_gray, y_test)
+score = model.evaluate_generator(
+    test_generator,
+    steps=validation_steps,
+    workers=0)
 print('test_loss:', score[0])
 print('test_acc:', score[1])
-
 
 # 訓練の推移
 fig = plt.figure(figsize=(10, 5))
@@ -136,48 +158,27 @@ ax.legend()  # 凡例を表示する
 
 plt.show()
 
-"""
-keras Functinal API での流れ
-①Input()            入力の作成
-②x = f(x)         　層の追加・出力の作成
-③model = Model()    箱の作成
-④model.compile()     設定
-⑤model.fit()  　　　  訓練
-⑥model.evaluate()　　 評価
-"""
-
-inputs = layers.Input(shape=(64*64,))
-x = layers.Dense(512, activation='relu')(inputs)
-x = layers.Dense(512, activation='relu')(x)
-x = layers.Dense(512, activation='relu')(x)
-x = layers.Dense(512, activation='relu')(x)
-x = layers.Dense(512, activation='relu')(x)
-x = layers.Dense(101, activation='softmax')(x)
-
-model = Model(inputs=inputs, outputs=x)
-
-# 以降は同じ
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])  # metrics=評価関数、acc=accuracy
-
-history = model.fit(x=x_train_gray, y=y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-
-score = model.evaluate(x_test_gray, y_test)
-print('test_loss:', score[0])
-print('test_acc:', score[1])
-
-fig = plt.figure(figsize=(10, 5))
-ax = fig.add_subplot(121)
-ax.plot(range(epochs), history.history['acc'], label='training')
-ax.plot(range(epochs), history.history['val_acc'], label='validation')
-ax.set_title('acc')
-ax.legend()
-ax = fig.add_subplot(122)
-ax.plot(range(epochs), history.history['loss'], label='training')
-ax.plot(range(epochs), history.history['val_loss'], label='validation')
-ax.set_title('loss')
-ax.legend()
-plt.show()
-
-
 # いろんなハイパラを全てハードコーディングしていますが、
 # 通常はプログラムの最初にまとめたり、ハイパラだけをまとめたファイルから読み込んだりします。
+
+
+# labels = []
+# with open(os.path.join(data_path, "labels.txt")) as file:
+#     line = file.readline()[:-1]
+#     labels.append(line)
+
+
+# train_path = os.path.join(data_path, "train")  # train-data
+# test_path = os.path.join(data_path, "test")  # test-data
+# meta_path = os.path.join(data_path, "meta")  # test-data
+
+# def unpickle(file):
+#     import pickle
+#     with open(file, 'rb') as fo:
+#         dict = pickle.load(fo, encoding='bytes')
+#     return dict
+#
+#
+# meta = unpickle(meta_path)
+# train = unpickle(train_path)
+# test = unpickle(test_path)
