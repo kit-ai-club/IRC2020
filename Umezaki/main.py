3ed74b10bae5aab783b0e9c8b2d62a36af248f83
kengo_Yoshida <kengo.y0413@gmail.com>
2020-07-09 21:28:16 +0900

    mainのprototype適応&ResNetの実装by吉田

--- Umezaki/main.py
+++ Umezaki/main.py
@@ -1,3 +1,10 @@
+"""
+【import構文とライブラリ】
+ライブラリ：パッケージをまとめたものであって、Anaconda とかを使ってインストールできるようにしたもの。numpy、keras など。
+パッケージ：.py（プログラムファイル）をまとめたディレクトリ（フォルダ）であって、 __init__.py  を放り込んだもの。
+　パッケージの中にあるものは、importできる。
+　自分のプロジェクト内のディレクトリにも、__init__.py というファイルがあれば、それをパッケージと認識させることができ、中のモジュール等をimportできる。
+"""
 import os
 import matplotlib.pyplot as plt
 import h5py
@@ -12,37 +19,27 @@ from keras.losses import categorical_crossentropy
 from keras.layers import GlobalAveragePooling2D
 from keras import regularizers
 import keras.layers as layers
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import glob
+import gc
+
+# pycharmでは、F4 を押すと、引用元・定義元に飛べるので使ってみよう
+
 
 """
-ハイパラ調整
+data path
 """
-epochs = 10
-batch_size = 100
-f = 64 #ここも変えるべきだろうか？
-"""
-データの取得
-"""
-# data-fileの場所（kaggleからDLする）
-data_path = os.path.join("drive", "My Drive")
-train_h5_path = os.path.join(data_path, "food_c101_n10099_r64x64x3 (1).h5")  # train-data   pathの文字列を確認してみよう
-test_h5_path = os.path.join(data_path, "food_test_c101_n1000_r64x64x3 (1).h5")  # test-data
-# windowsとlinuxで、スラッシュとバックスラッシュの違いがあることを気にしないために、 os.path.join を使う
-# train-data
-with h5py.File(train_h5_path, 'r') as file:  # train-dataを 'r' = read mode で読み込んで、変数fileに格納
-    print(file.keys())  # .h5 形式では、関数keys()で中身が見れる
+data_path = os.path.join("data")  # colabの場合、google-driveのルートが、"drive/My Drive" となる
+train_path = os.path.join(data_path, "train")
+test_path = os.path.join(data_path, "test")
+
+# example
+ex_path = os.path.join(train_path, "train_256_0.h5")
+with h5py.File(ex_path, 'r') as file:  # train-dataを 'r' = read mode で読み込んで、変数fileに格納
     x_train = file['images'].value  # imagesにアクセス。さらに、.value と書くとnumpy形式にしてくれる
     y_train = file['category'].value  # 教師データ（正解データ）。すでにone-hot vector になっている
-    category_names = file['category_names'].value  # str型ではなく、bytes型で格納されている。見てみよう
-    label_names = [x.decode() for x in file['category_names'].value]  # 全カテゴリ名を格納。bytes型は、decode()でstrに変換できる。
-    print(label_names)
-# with構文で開いたファイルは、構文が終われば自動的に閉じられる。
-# x_train = (10099, 64, 64, 3), y_train = (10099, 101)
-#  ↑ このようにshapeを調べてメモしておくと便利
-# test-data
-with h5py.File(test_h5_path, 'r') as file:
-    print(file.keys())
-    x_test = file['images'].value
-    y_test = file['category'].value
+
 """
 【matplotlib.pyplotの基本】
 ※pyplotはpltとしてimportしておく
@@ -51,72 +48,24 @@ with h5py.File(test_h5_path, 'r') as file:
 ③axes上にグラフを作成する（axes.plot()なら折れ線グラフ、など）
 ④show
 """
-
-
 # 画像チェック
-fig = plt.figure(figsize=(10, 5))  # figure-sizeはインチ単位
-ax = fig.add_subplot(121)  # Figure内にAxesを追加。121 =「1行2列のaxesを作って、その1番目(1列目)をreturnしろ」
+fig = plt.figure(figsize=(3, 3))  # figure-sizeはインチ単位
+ax = fig.add_subplot(111)  # Figure内にAxesを追加。121 =「1行2列のaxesを作って、その1番目(1列目)をreturnしろ」
 ax.imshow(x_train[0])  # 画像ならimshow()
-# 最後はpltに戻る
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+plt.show()  # 最後はpltに戻る
 
 """
-sequentialモデル一旦グッバイ
-model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', input_shape=(64, 64, 3)))
-model.add(BatchNormalization())
-model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
-model.add(Conv2D(128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
-model.add(Conv2D(256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
-model.add(Conv2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
-model.add(GlobalAveragePooling2D())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(101, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-
-
-# 訓練の実行
-history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-# historyに訓練の推移のデータが格納される
-# 評価
-score = model.evaluate(x_test, y_test)
-print('test_loss:', score[0])
-print('test_acc:', score[1])
-# 訓練の推移
-fig = plt.figure(figsize=(10, 5))
-ax = fig.add_subplot(121)
-ax.plot(range(epochs), history.history['accuracy'], label='training')  # x軸、y軸、ラベル
-ax.plot(range(epochs), history.history['val_accuracy'], label='validation')
-ax.set_title('accuracy')
-ax.legend()  # 凡例を表示する
-ax = fig.add_subplot(122)
-ax.plot(range(epochs), history.history['loss'], label='training')
-ax.plot(range(epochs), history.history['val_loss'], label='validation')
-ax.set_title('loss')
-ax.legend()  # 凡例を表示する
-plt.show()
+keras Functinal API での流れ
+①Input()            入力の作成
+②x = f(x)         　層の追加・出力の作成
+③model = Model()    箱の作成
+④model.compile()     設定
+⑤model.fit()  　　　  訓練
+⑥model.evaluate()　　 評価
 """
 
-
-"""
-resnetの部分
-"""
 inputs = Input(shape=x_train.shape[1:])
+f = 64
 ki = 'he_normal'
 kr = regularizers.l2(1e-11)
 x = Conv2D(filters=f, kernel_size=7, padding='same', kernel_initializer=ki, kernel_regularizer=kr)(inputs)
@@ -148,24 +97,115 @@ x = Dropout(rate=0.4)(x)
 
 model = Model(inputs=inputs, outputs=x)
 
-# 以降は同じ
+# 各種設定
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])  # metrics=評価関数、acc=accuracy
 
-history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+# メモリリーク対策のグローバル変数
+x, y, datagen, generator = None, None, None, None  # いじらない
 
-score = model.evaluate(x_test, y_test)
+
+def flow_from_h5(directory, batch_size, data_aug=False):
+    """
+    directory内のh5ファイルを順に読み込んで出力するジェネレータ。
+    ImageDataGeneratorの部分以外はいじらない。
+    """
+
+    files = glob.glob(os.path.join(directory, '*.h5'))
+    while True:
+        for file in files:
+            global x, y, datagen, generator
+            del x, y, datagen, generator
+            gc.collect()
+
+            with h5py.File(file, 'r') as f:
+                x = f['images'].value
+                y = f['category'].value
+
+            if not data_aug:
+                datagen = ImageDataGenerator(rescale=1 / 255.)  # いじらない。rescaleで画像を正規化している。
+            else:
+                datagen = ImageDataGenerator(rescale=1 / 255.)  # DataAugmentationするなら、引数（rescale以外）をいじる。
+
+            generator = datagen.flow(
+                x,
+                y,
+                batch_size=batch_size,
+                shuffle=True, )
+            epoch_per_file = x.shape[0] // batch_size + 1
+            for e in range(epoch_per_file):
+                yield next(generator)
+
+
+"""
+画像を256*256にしてから激重なので、pycharm上でデバッグしたいときは、
+以下4つの値を全て小さめに設定すれば、動きが確認できる程度に軽くなるはず（colabで訓練するときは値を戻すことに注意）
+"""
+epochs = 100
+batch_size = 100
+steps_per_epoch = 75750 // batch_size + 1  # 元は 75750 // batch_size + 1
+validation_steps = 25250 // batch_size + 1  # 元は  25250 // batch_size + 1
+
+train_generator = flow_from_h5(train_path, batch_size, data_aug=True)
+test_generator = flow_from_h5(test_path, batch_size, data_aug=False)
+
+"""
+以降は、原則いじらない
+"""
+# 訓練
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=steps_per_epoch,
+    epochs=epochs,
+    validation_data=test_generator,
+    validation_steps=validation_steps,
+    workers=0)
+
+# 評価
+score = model.evaluate_generator(
+    test_generator,
+    steps=validation_steps,
+    workers=0)
 print('test_loss:', score[0])
 print('test_acc:', score[1])
 
+# 訓練の推移
 fig = plt.figure(figsize=(10, 5))
+
 ax = fig.add_subplot(121)
-ax.plot(range(epochs), history.history['acc'], label='training')
+ax.plot(range(epochs), history.history['acc'], label='training')  # x軸、y軸、ラベル
 ax.plot(range(epochs), history.history['val_acc'], label='validation')
 ax.set_title('acc')
-ax.legend()
+ax.legend()  # 凡例を表示する
+
 ax = fig.add_subplot(122)
 ax.plot(range(epochs), history.history['loss'], label='training')
 ax.plot(range(epochs), history.history['val_loss'], label='validation')
 ax.set_title('loss')
-ax.legend()
+ax.legend()  # 凡例を表示する
+
 plt.show()
+
+# いろんなハイパラを全てハードコーディングしていますが、
+# 通常はプログラムの最初にまとめたり、ハイパラだけをまとめたファイルから読み込んだりします。
+
+
+# labels = []
+# with open(os.path.join(data_path, "labels.txt")) as file:
+#     line = file.readline()[:-1]
+#     labels.append(line)
+
+
+# train_path = os.path.join(data_path, "train")  # train-data
+# test_path = os.path.join(data_path, "test")  # test-data
+# meta_path = os.path.join(data_path, "meta")  # test-data
+
+# def unpickle(file):
+#     import pickle
+#     with open(file, 'rb') as fo:
+#         dict = pickle.load(fo, encoding='bytes')
+#     return dict
+#
+#
+# meta = unpickle(meta_path)
+# train = unpickle(train_path)
+# test = unpickle(test_path)
